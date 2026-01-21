959f7bf687827ac3e054fdc47c1b2960d1a351a1
RobHa93 <robin.hasler@bluewin.ch>
2026-01-21 19:20:49 +0100

    Logo einbauen

---
+++ src/components/LogosRow.jsx
@@ -0,0 +1,21 @@
+import daciaLogo from '../assets/img/dacia-logo.webp';
+import renaultLogo from '../assets/img/Renault-logo.webp';
+
+const LogosRow = () => (
+  <div className=" bg-gray-50 flex justify-center items-start gap-12 my-8 mb-4">
+    <div className="flex flex-col items-center">
+      <img src={renaultLogo} alt="Renault Logo" className="h-16 w-auto object-contain mb-2" />
+      <div className="text-gray-900 font-semibold text-lg bg-white px-2 py-1 rounded mt-1">
+        Verkauf, Werkstatt, Service
+      </div>
+    </div>
+    <div className="flex flex-col items-center">
+      <img src={daciaLogo} alt="Dacia Logo" className=" mt-6 h-10 w-auto object-contain mb-2" />
+      <div className="text-gray-900 font-semibold text-lg bg-white px-2 py-1 rounded mt-1">
+        Verkauf, Werkstatt, Service
+      </div>
+    </div>
+  </div>
+);
+
+export default LogosRow;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,14 @@ import NewsSlider from '../components/NewsSlider'
 import ServicesSection from '../components/ServicesSection'
 import Carousel from '../components/carouselBrands'
 
+import LogosRow from '../components/LogosRow'
+
 const Home = () => {
   return (
     <>
       <HeroSection />
       <NewsSlider />
+      <LogosRow />
       <ServicesSection />
       <Carousel />
     </>
